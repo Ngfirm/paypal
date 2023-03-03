d7fe3156c3728e6c0fbb3062d00a7e56f56429d9
pascalgy25 <126115511+pascalgy25@users.noreply.github.com>
2023-03-02 22:45:03 -0600

    Bug  removed

--- app.java
+++ app.java
@@ -1,6 +1,7 @@
-we support ;
-we love our friends
-developing java apps
+we support,
+we love our friends,
+developing java apps,
 .net applications
 NodeJS applications 
 python application
+bug removed
